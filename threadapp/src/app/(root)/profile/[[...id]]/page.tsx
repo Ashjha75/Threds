6bb032b6cc84a34276fb8eaa6d38a8ce3cea9594
AshishJha <ajha5645@gmail.com>
2023-09-08 10:16:39 +0530

    26 commit

--- threadapp/src/app/(root)/profile/[[...id]]/page.tsx
+++ threadapp/src/app/(root)/profile/[[...id]]/page.tsx
@@ -1,107 +1,25 @@
 import ProfileTab from "@/app/components/shared/ProfileTab";
-import { fetchUsersData } from "@/lib/helpers/UserData";
-import { formatCreatedAtDate } from "@/lib/helpers/commonFunctions";
-import Image from "next/image";
-import { SlCalender } from "react-icons/sl";
-import ThreadCard from "@/app/components/cards/ThreadCard";
-import Posts from "@/app/components/shared/threads";
+import ProfileData from "@/app/components/shared/profileData";
+import { fetchByUsername, fetchUserByName } from "@/lib/actions/user.actions";
+import { decodeToken } from "@/lib/helpers/tokenData";
+import mongoose, { Types } from "mongoose";
+
 const page = async ({ params }: { params: { id: string } }) => {
-  const userData = await fetchUsersData(params.id);
+  const userData = await fetchByUsername(params.id);
+  const currentUser = await decodeToken();
+  const currentUserCheck = userData._id == currentUser;
+  const check = userData.followers
+    .map((id: Types.ObjectId) => id.toString())
+    .includes(currentUser.toString());
+
   return (
     <section className="text-light-1">
-      <div className="relative ">
-        <img
-          src="/assets/bg.jpg"
-          alt="back Photo"
-          className="w-full h-40  rounded-sm object-cover"
-        />
-      </div>
-      <div className="flex  mb-10">
-        <div className="flex-1">
-          <Image
-            src="/assets/user-2.png"
-            alt={"userData.name"}
-            width={120}
-            height={120}
-            className="rounded-full shadow-md cursor-pointer absolute top-[15rem]  border-4 border-[#193549]"
-          />
-        </div>
-        <div className="">
-          <button className="bg-transparent rounded-3xl mt-4 text-small-regular  border-[1px] border-gray-300 py-2 px-6  hover:bg-[#2c3640]">
-            Edit Profile
-          </button>
-        </div>
-      </div>
-      <article className="ml-8">
-        <strong>{userData.name}</strong>
-        <div className="text-primary-500 ">@{userData.username}</div>
-        <div className="mt-5">{userData.bio}</div>
-        <div className="text-gray-400 flex gap-x-4 items-center text-small-regular">
-          <SlCalender />
-          Joined {formatCreatedAtDate(userData.createdAt)}
-        </div>
-      </article>
-      <div className="text-light-1">Posts</div>
-
-      <ProfileTab userData={params.id} />
-      {userData.threads.reverse().map((post: any) => (
-        <div className="mt-5">
-          <Posts
-            key={post._id}
-            id={post._id}
-            currentUserId={"64ddb6dfa72514aea656f27c"}
-            parentId={post.parentId}
-            content={post.content}
-            author={userData.username}
-            community={post.community}
-            createdAt={post.createdAt}
-            contents={post.author.username}
-            commentCount={post.childrenCount}
-            likeCount={post.likeCount}
-            type={"COMMENT"}
-            isComment={false}
-          />
-        </div>
-      ))}
-      <div className="text-light-1">replies</div>
-
-      {userData.replies.reverse().map((post: any) => (
-        <div className="mt-5">
-          <Posts
-            key={post._id}
-            id={post._id}
-            currentUserId={"64ddb6dfa72514aea656f27c"}
-            parentId={post.parentId}
-            content={post.content}
-            author={userData.username}
-            community={post.community}
-            createdAt={post.createdAt}
-            contents={post.author.username}
-            commentCount={post.childrenCount}
-            likeCount={post.likeCount}
-            type={"COMMENT"}
-            isComment={false}
-          />
-        </div>
-      ))}
-      <div className="text-light-1">Likes</div>
-      {userData.likes.reverse().map((post: any) => (
-        <Posts
-          key={post._id}
-          id={post._id}
-          currentUserId={"64ddb6dfa72514aea656f27c"}
-          parentId={post.parentId}
-          content={post.content}
-          author={userData.username}
-          community={post.community}
-          createdAt={post.createdAt}
-          contents={post.author.username}
-          commentCount={post.childrenCount}
-          likeCount={post.likeCount}
-          type={"COMMENT"}
-          isComment={false}
-        />
-      ))}
+      <ProfileData
+        userData={userData}
+        check={check}
+        currentUserCheck={currentUserCheck}
+        currentUser={currentUser}
+      />
     </section>
   );
 };
