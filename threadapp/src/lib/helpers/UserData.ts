629914bd8d8d9fb191c45abe9c868386185b8879
AshishJha <ajha5645@gmail.com>
2023-09-10 11:23:12 +0530

    28 commit

--- threadapp/src/lib/helpers/UserData.ts
+++ threadapp/src/lib/helpers/UserData.ts
@@ -1,14 +1,78 @@
 "use server";
 
+import Thread from "../models/thread.model";
 import User from "../models/user.model";
 
-export const fetchUsersData = (username: any) => {
+export const fetchUsersData = async (username: string) => {
     try {
-        const userDatas = User.findOne({ username: username }).populate(
-            "threads"
+        const userDatas = await User.findOne({ username: username }).populate(
+            {
+                path: "threads",
+                model: Thread,
+                populate: {
+                    path: 'children',
+                    model: Thread,
+                    select: 'content ',
+                    populate: {
+                        path: 'author',
+                        model: User,
+                        select: 'name _id username'
+                    }
+                }
+            }
+        ).populate(
+            {
+                path: "replies",
+                model: Thread,
+
+                populate: {
+                    path: 'author',
+                    model: User,
+                    select: 'name _id username'
+                }
+            }
+        ).populate(
+            {
+                path: "likes",
+                model: Thread,
+
+                populate: {
+                    path: 'author',
+                    model: User,
+                    select: 'name _id username'
+                }
+            }
         ).exec()
+
         return userDatas;
     } catch (error: any) {
         throw new Error(error.message);
     }
+}
+
+
+export const fetchUsersPosts = async (username: string) => {
+    try {
+        const userDatas = await User.findOne({ username: username }).populate(
+            {
+                path: "threads",
+                model: Thread,
+                populate: {
+                    path: 'children',
+                    model: Thread,
+                    select: 'content ',
+                    populate: {
+                        path: 'author',
+                        model: User,
+                        select: 'name _id username'
+                    }
+                }
+            }
+        ).exec()
+        return userDatas;
+    }
+    catch (error: any) {
+        throw new Error(error.message);
+    }
+
 }
